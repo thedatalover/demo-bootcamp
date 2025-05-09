ce513f8a18c145f867a6e52fc22d3e7f0f9d0952
jarturomora <jarturomora@gmail.com>
2025-05-09 19:53:48 +0200

    Actuliza los scripts de la suma

--- suma.py
+++ suma.py
@@ -1,8 +1,12 @@
 # suma.py
 
 def sumar(a, b):
-   # Esta función suma dos números. 
-   return a + b
+   # Esta función suma dos números.
+   resultado = a + b
+   # Devuelve el resultado.
+   return resultado
 
 if __name__ == "__main__":
-    print(sumar(5, 2))
+    print("Ejecutando la función suma")
+    suma = sumar(5, 2)
+    print("El resultado de la suma es:", suma)
--- test_suma.py
+++ test_suma.py
@@ -9,7 +9,7 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
-        self.assertEqual(sumar(4, 2), 6)
+        self.assertEqual(sumar(4.1, 2), 6.1)
         self.assertEqual(sumar(14, 2), 16)
 
 if __name__ == '__main__':
